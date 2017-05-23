25ab954fc27c54602abf8791414311a9cd67e1ac
Christopher Gupta <gupta.christopher@gmail.com>
2017-05-23 16:09:52 -0400

    more backend tests

--- db/db.test.js
+++ db/db.test.js
@@ -1,9 +1,11 @@
 const chai = require('chai');
+const supertest = require('supertest-as-promised')
 const expect = chai.expect;
 
 const Technical = require('./technical')
 const General = require('./general')
 const Intro = require('./intro')
+const app = require('../server/main')
 
 const axios = require('axios')
 const Promise = require('bluebird')
@@ -12,19 +14,20 @@ let questions1, questions2, questions3
 
 describe('Backend tests', () => {
 
+  let agent
+
   beforeEach('grab the question', () => {
-    return Promise.all(
-      axios.get('api/general')
-      .then((res) => res.data),
-      axios.get('api/intro')
-      .then((res) => res.data),
-      axios.get('api/technical')
-      .then((res) => res.data))
+    agent = supertest(app)
+    let p1 = agent.get('/api/general').then((res) => res.data)
+    let p2 = agent.get('/api/intro').then((res) => res.data)
+    let p3 = agent.get('/api/technical').then((res) => res.data)
+    return Promise.all([p1, p2, p3])
       .spread((general, intro, technical) => {
         questions1 = general;
         questions2 = intro;
         questions3 = technical
       })
+      .catch(err => console.error(err))
     })
 
     describe('routes for questions', () => {
